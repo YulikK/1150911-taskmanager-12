c033b727e062f496625c872013e4b027fcc7a541
Yulik <yulikpoch@gmail.com>
2020-09-18 17:31:34 +0300

    Связь с сервером

--- src/main.js
+++ src/main.js
@@ -1,30 +1,25 @@
 import SiteMenuView from "./view/site-menu.js";
 import StatisticsView from "./view/statistics.js";
-import {generateTask} from "./mock/task.js";
 import BoardPresenter from "./presenter/board.js";
 import FilterPresenter from "./presenter/filter.js";
 import TasksModel from "./model/tasks.js";
 import FilterModel from "./model/filter.js";
 import {render, RenderPosition, remove} from "./utils/render.js";
 import {MenuItem, UpdateType, FilterType} from "./const.js";
+import Api from "./api.js";
 
-const TASK_COUNT = 22;
-
-const tasks = new Array(TASK_COUNT).fill().map(generateTask);
-
-const tasksModel = new TasksModel();
-
-tasksModel.setTasks(tasks);
-
-const filterModel = new FilterModel();
+const AUTHORIZATION = `Basic hu2839uhwi3hed94w`;
+const END_POINT = `https://12.ecmascript.pages.academy/task-manager`;
 
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
-const siteMenuComponent = new SiteMenuView();
-render(siteHeaderElement, siteMenuComponent, RenderPosition.BEFOREEND);
+const api = new Api(END_POINT, AUTHORIZATION);
 
-const boardPresenter = new BoardPresenter(siteMainElement, tasksModel, filterModel);
+const tasksModel = new TasksModel();
+const filterModel = new FilterModel();
+const siteMenuComponent = new SiteMenuView();
+const boardPresenter = new BoardPresenter(siteMainElement, tasksModel, filterModel, api);
 const filterPresenter = new FilterPresenter(siteMainElement, filterModel, tasksModel);
 
 const handleTaskNewFormClose = () => {
@@ -56,7 +51,17 @@ const handleSiteMenuClick = (menuItem) => {
   }
 };
 
-siteMenuComponent.setMenuClickHandler(handleSiteMenuClick);
-
 filterPresenter.init();
 boardPresenter.init();
+
+api.getTasks()
+  .then((tasks) => {
+    tasksModel.setTasks(UpdateType.INIT, tasks);
+    render(siteHeaderElement, siteMenuComponent, RenderPosition.BEFOREEND);
+    siteMenuComponent.setMenuClickHandler(handleSiteMenuClick);
+  })
+  .catch(() => {
+    tasksModel.setTasks(UpdateType.INIT, []);
+    render(siteHeaderElement, siteMenuComponent, RenderPosition.BEFOREEND);
+    siteMenuComponent.setMenuClickHandler(handleSiteMenuClick);
+  });
--- src/view/task-edit.js
+++ src/view/task-edit.js
@@ -6,7 +6,7 @@ import flatpickr from "flatpickr";
 
 import "../../node_modules/flatpickr/dist/flatpickr.min.css";
 
-const createTaskEditDateTemplate = (dueDate, isDueDate) => {
+const createTaskEditDateTemplate = (dueDate, isDueDate, isDisabled) => {
   return `<button class="card__date-deadline-toggle" type="button">
       date: <span class="card__date-status">${isDueDate ? `yes` : `no`}</span>
     </button>
@@ -18,13 +18,14 @@ const createTaskEditDateTemplate = (dueDate, isDueDate) => {
           placeholder=""
           name="date"
           value="${formatTaskDueDate(dueDate)}"
+          ${isDisabled ? `disabled` : ``}
         />
       </label>
     </fieldset>` : ``}
   `;
 };
 
-const createTaskEditRepeatingTemplate = (repeating, isRepeating) => {
+const createTaskEditRepeatingTemplate = (repeating, isRepeating, isDisabled) => {
   return `<button class="card__repeat-toggle" type="button">
     repeat:<span class="card__repeat-status">${isRepeating ? `yes` : `no`}</span>
   </button>
@@ -37,6 +38,7 @@ const createTaskEditRepeatingTemplate = (repeating, isRepeating) => {
         name="repeat"
         value="${day}"
         ${repeat ? `checked` : ``}
+        ${isDisabled ? `checked` : ``}
       />
       <label class="card__repeat-day" for="repeat-${day}"
         >${day}</label
@@ -63,15 +65,25 @@ const createTaskEditColorsTemplate = (currentColor) => {
 };
 
 const createTaskEditTemplate = (data) => {
-  const {color, description, dueDate, repeating, isDueDate, isRepeating} = data;
+  const {
+    color,
+    description,
+    dueDate,
+    repeating,
+    isDueDate,
+    isRepeating,
+    isDisabled,
+    isSaving,
+    isDeleting
+  } = data;
 
-  const dateTemplate = createTaskEditDateTemplate(dueDate, isDueDate);
+  const dateTemplate = createTaskEditDateTemplate(dueDate, isDueDate, isDisabled);
 
   const repeatingClassName = isRepeating
     ? `card--repeat`
     : ``;
 
-  const repeatingTemplate = createTaskEditRepeatingTemplate(repeating, isRepeating);
+  const repeatingTemplate = createTaskEditRepeatingTemplate(repeating, isRepeating, isDisabled);
 
   const colorsTemplate = createTaskEditColorsTemplate(color);
 
@@ -92,6 +104,7 @@ const createTaskEditTemplate = (data) => {
                 class="card__text"
                 placeholder="Start typing your text here..."
                 name="text"
+                ${isDisabled ? `disabled` : ``}
               >${he.encode(description)}</textarea>
             </label>
           </div>
@@ -110,8 +123,8 @@ const createTaskEditTemplate = (data) => {
             </div>
           </div>
           <div class="card__status-btns">
-            <button class="card__save" type="submit" ${isSubmitDisabled ? `disabled` : ``}>save</button>
-            <button class="card__delete" type="button">delete</button>
+            <button class="card__save" type="submit" ${isSubmitDisabled || isDisabled ? `disabled` : ``}>${isSaving ? `saving...` : `save`}</button>
+            <button class="card__delete" type="button"${isDisabled ? `disabled` : ``}>${isDeleting ? `deleting...` : `delete`}</button>
           </div>
         </div>
       </form>
@@ -280,7 +293,10 @@ export default class TaskEdit extends SmartView {
         task,
         {
           isDueDate: task.dueDate !== null,
-          isRepeating: isTaskRepeating(task.repeating)
+          isRepeating: isTaskRepeating(task.repeating),
+          isDisabled: false,
+          isSaving: false,
+          isDeleting: false
         }
     );
   }
@@ -306,6 +322,9 @@ export default class TaskEdit extends SmartView {
 
     delete data.isDueDate;
     delete data.isRepeating;
+    delete data.isDisabled;
+    delete data.isSaving;
+    delete data.isDeleting;
 
     return data;
   }
